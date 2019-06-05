c72519049e0fbddedf7ed813bf445e3d0eba3ef1
Deepak Bhari <deepak.bhari1@Deepaks-MBP.lan>
2019-06-05 15:24:06 +0100

    put language selector into seperate LanguageSelector component

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,29 @@
-import React from 'react';
-import UserCreate from './UserCreate';
-import LanguageContext from '../contexts/LanguageContext';
-import ColorContext from '../contexts/ColorContext';
+import React from "react";
+import UserCreate from "./UserCreate";
+import LanguageContext from "../contexts/LanguageContext";
+import ColorContext from "../contexts/ColorContext";
+import LanguageSelector from "./LanguageSelector";
 
 class App extends React.Component {
+  state = { language: "english", color: "primary" };
 
-    state = { language: 'english', color: 'primary' };
+  onLanguageChange = (language, color) => {
+    this.setState({ language, color });
+  };
 
-    onLanguageChange = (language, color) => {
-        this.setState({ language, color });
-    }
+  render() {
+    return (
+      <div className="ui container">
+        <LanguageSelector onLanguageChange={this.onLanguageChange}/>
 
-    render() {
-        return (
-            <div className="ui container">
-                <div>Select a language:
-                    <i className="flag us" onClick={() => this.onLanguageChange('english', 'primary')} />
-                    <i className="flag nl" onClick={() => this.onLanguageChange('dutch', 'red')} />
-                </div>
-
-                <ColorContext.Provider value={this.state.color}>
-                    <LanguageContext.Provider value={this.state.language}>
-                        <UserCreate />
-                    </LanguageContext.Provider>
-                </ColorContext.Provider>
-
-            </div>
-        )
-    }
+        <ColorContext.Provider value={this.state.color}>
+          <LanguageContext.Provider value={this.state.language}>
+            <UserCreate />
+          </LanguageContext.Provider>
+        </ColorContext.Provider>
+      </div>
+    );
+  }
 }
 
 export default App;
